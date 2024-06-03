7ff7a4258eb9b53d1a0becae0f4b285371363e9b
chris <chris@csie.io>
2024-06-03 18:11:10 +0800

    [Chore] Remove redundant items

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,9 @@ import { ServerStatusModule } from './server-status/server-status.module';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { DatabaseModule } from './database/database.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users/user.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User]),
     DatabaseModule,
     ServerStatusModule,
     UsersModule,
